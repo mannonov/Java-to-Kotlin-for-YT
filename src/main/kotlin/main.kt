a8b43f0d1a89bb3340f4b2035323dd34ca4af2ae
mannonov <jahongir.sh.mannonov@gmail.com>
2021-07-28 10:07:12 +0500

    Kotlin Type Conversion

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,20 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+    val value = 4546546.4545F
+
+//    val char: CharArray = value.toCharArray()
+
+
+    val string = value.toString()
+
+    println(string)
+
+//    val number: Int = value.toInt()
+//
+//    for (i in char.iterator()){
+//        println(i)
+//    }
+
+
+
 }
