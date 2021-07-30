bbfdb759490b942aa59dac11bb1e76ac94d7a86e
mannonov <jahongir.sh.mannonov@gmail.com>
2021-07-30 10:41:32 +0500

    Kotlin expression

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,13 @@
 fun main(args: Array<String>) {
 
-    val value = 4546546.4545F
+//    val value = 4546546.4545F
 
 //    val char: CharArray = value.toCharArray()
 
 
-    val string = value.toString()
-
-    println(string)
+//    val string = value.toString()
+//
+//    println(string)
 
 //    val number: Int = value.toInt()
 //
@@ -16,5 +16,34 @@ fun main(args: Array<String>) {
 //    }
 
 
+    val number: Int = 5
+
+    val str: String = when (number) {
+        0 -> "number is 0"
+        1 -> "number is 1"
+        2 -> "number is 2"
+        3 -> "number is 3"
+        4 -> "number is 4"
+        5 -> "number is 5"
+        else -> "number not found"
+    }
+
+    println(str)
+
+    val a = 15
+    val b = 16
+
+    val max: Int = if (a > b) a else b
+
+//
+//    if (a > b){
+//
+//        max = a
+//
+//    }else{
+//        max = b
+//    }
+
+    println(max)
 
 }
