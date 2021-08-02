c7bd44e8dd5d5d51c9f2ead916c0ed2405195167
mannonov <jahongir.sh.mannonov@gmail.com>
2021-08-02 14:25:21 +0500

    for, array

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 fun main(args: Array<String>) {
 
 //    val value = 4546546.4545F
@@ -16,24 +18,24 @@ fun main(args: Array<String>) {
 //    }
 
 
-    val number: Int = 5
-
-    val str: String = when (number) {
-        0 -> "number is 0"
-        1 -> "number is 1"
-        2 -> "number is 2"
-        3 -> "number is 3"
-        4 -> "number is 4"
-        5 -> "number is 5"
-        else -> "number not found"
-    }
-
-    println(str)
-
-    val a = 15
-    val b = 16
-
-    val max: Int = if (a > b) a else b
+//    val number: Int = 5
+//
+//    val str: String = when (number) {
+//        0 -> "number is 0"
+//        1 -> "number is 1"
+//        2 -> "number is 2"
+//        3 -> "number is 3"
+//        4 -> "number is 4"
+//        5 -> "number is 5"
+//        else -> "number not found"
+//    }
+//
+//    println(str)
+//
+//    val a = 15
+//    val b = 16
+//
+//    val max: Int = if (a > b) a else b
 
 //
 //    if (a > b){
@@ -44,6 +46,25 @@ fun main(args: Array<String>) {
 //        max = b
 //    }
 
-    println(max)
+//    println(max)
+
+
+    //for(int i =55; i >= 0; i++){
+    //
+//        println(i)
+    //
+    // }
+
+
+    val array = intArrayOf(454,454,4545,454,545,454,545,45,4,54,54,5,45,45,)
+
+    val array1 = arrayListOf("jaxadev","Youtube",5544,'c')
+
+    for (i in arrayOf("String",454,'c',45.54F)) {
+
+        println(i)
+
+    }
+
 
 }
